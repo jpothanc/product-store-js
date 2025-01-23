cba8346a58b501fcc24339645f38ff8a8eb0db0e
jpothanc <70871841+jpothanc@users.noreply.github.com>
2025-01-23 19:44:34 +0800

    Add test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
 export default {
-  transform: {
-    "^.+\\.js$": "babel-jest", // Transpile .js files using Babel-Jest
-  },
+  //   transform: {
+  //     "^.+\\.js$": "babel-jest", // Transpile .js files using Babel-Jest
+  //   },
+  collectCoverage: true, // Enable code coverage
+  collectCoverageFrom: [
+    "src/**/*.js", // Include all JS files in the src directory
+    "!src/**/*.test.js", // Exclude test files
+    "!src/index.js", // Exclude entry points if needed
+  ],
+  coverageDirectory: "coverage", // Directory for coverage output
+  coverageReporters: ["json", "text", "lcov"], // Generate HTML, text, and JSON reports
   testEnvironment: "node",
 
   moduleNameMapper: {
